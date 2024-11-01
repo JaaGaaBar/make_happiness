be5666377730afda45ddfbefd90903f1f99fc255
JGB <yasuhisa64@outlook.jp>
2024-11-01 22:52:28 +0900

    Update and rename happy.py to happiness.py

--- happiness.py
+++ happiness.py
@@ -2,8 +2,8 @@ def happy():
     ha = ['136972 187130 ' * (136972 + 187130)]
     print(ha)
 
-def make_happy():
+def make_happiness():
     for i in range(132):
         happy()
 
-make_happy()
+make_happiness()
